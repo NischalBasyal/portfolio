110b9bc45c03021e1877174908236aa74499ddbe
Nischal Basyal <117506677+NischalBasyal@users.noreply.github.com>
2022-11-19 18:37:30 +0000

    main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Navbar } from './Components/Navbar'
 
 export default function Home() {
   return (
@@ -11,11 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     
+      <Navbar />
       <footer className={styles.footer}>
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
+          Contact Nischal Basyal here:<a href="https://chat.test.nischalbasyal.com">Trench Chat</a>
       </footer>
     </div>
   )
